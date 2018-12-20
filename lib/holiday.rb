4a86d290e92c68308445e8585f3a53b664cd1611
Joe Ganobsik <jganobsik@gmail.com>
2018-12-20 22:58:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, season_hash|
     puts "#{season.to_s.capitalize!}:"
     season_hash.each do |holiday, items_hash|
-      puts "#{holiday.to_s.capitalize!}: #{items_hash.flatten!} "
+      items_store = items_hash
+      puts "#{holiday.to_s.capitalize!}: #{items_store.flatten} "
     end
   end
 
